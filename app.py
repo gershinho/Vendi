12be07a5e326b29407b6b0448f0408ef673ab687
gershinho <kpatel6607@gmail.com>
2025-02-28 21:16:03 -0600

    update app

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/register_truck', methods=['POST', 'GET'])
+def register_truck():
+    if request.method == 'POST':
+        name = request.form['foodTruckName']
+        print(name)
+    return render_template('register_truck.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
